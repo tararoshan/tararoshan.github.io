664e4d9eb6712d982573c31c8c8b871619da5628
tararoshan <troshan@utexas.edu>
2022-05-22 18:06:44 -0500

    Initialize repository and CI workflow

---
+++ pages/404.js
@@ -0,0 +1,7 @@
+export default function Custom404() {
+    return (
+    <>
+        <h1>404 - Page Not Found</h1>
+        <p>Are you sure you typed the right address? Maybe this is probably a bug I need to fix... oops!</p>
+    </>);
+}
